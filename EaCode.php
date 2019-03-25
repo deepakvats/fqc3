babfc704b028784ed240b8d0bc70b70fddfbcad7
deepakvats <48901772+deepakvats@users.noreply.github.com>
2019-03-25 13:36:53 +0530

    aa

---
+++ EaCode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EaCode extends Model
+{
+	protected $table = 'ea_codes';
+    public $timestamps = false;
+
+}
---
+++ NaceCode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NaceCode extends Model
+{
+  protected $table = 'nace_codes';
+  public $timestamps = false;
+
+}
